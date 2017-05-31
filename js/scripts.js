3c65190a074f3b3a1bb9e7bdc6ddde249f54f36b
pinko <agaslota@wp.pl>
2017-05-31 08:33:04 +0100

    new comments in js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// scripts.js
+
+var name = prompt('Enter your name');
+var city = prompt('Where are you from?');
+// Thanks to my teacher I was able to do what I wanted :)
+alert('Hello, ' + name + ' from ' + city);
+console.log('Hello, ' + name + ' from ' + city + ' ale zobacze to tylko ja ');
+// test for checking how does it work
+var variable = 'test';
+console.log(variable);
+// I like js :)

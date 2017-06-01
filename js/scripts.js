023bf63a8e7e88d1aec3860f1cdba5f5339a218f
pinko <agaslota@wp.pl>
2017-06-01 19:53:25 +0100

    commented line 4

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,7 @@
 
 var name = prompt('Enter your name');
 var city = prompt('Where are you from?');
-// Thanks to my teacher I was able to do what I wanted :)
-alert('Hello, ' + name + ' from ' + city); // welcome to everybody
-console.log('Hello, ' + name + ' from ' + city + ' ale zobacze to tylko ja ');
+// I don't want that on my website right now: console.log('Hello, ' + name + ' from ' + city + ' ale zobacze to tylko ja ');
 // test for checking how does it work
 var variable = 'test';
 console.log(variable);

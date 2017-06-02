a30d2b969d93a1cb317d157fb1b2b1d0fdacf0e1
pinko <agaslota@wp.pl>
2017-06-02 07:21:21 +0100

    added window alert and a comment

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,8 @@
 
 var name = prompt('Enter your name');
 var city = prompt('Where are you from?');
-// I don't want that on my website right now: console.log('Hello, ' + name + ' from ' + city + ' ale zobacze to tylko ja ');
+// silly me, finaly it should be ok. alert('Your name ' + name + ' is very beautiful');
+console.log('Hello, ' + name + ' from ' + city + ' ale zobacze to tylko ja ');
 // test for checking how does it work
 var variable = 'test';
 console.log(variable);
